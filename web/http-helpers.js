d3a043a2330af0140a19849f01eeb4a520eeae66
rossman-andrew <andrew100r@gmail.com>
2017-09-15 20:01:44 -0700

    Passing get index.html test. Getting input from index form.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,6 +14,17 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  // fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', function(err, data){
+  //   //console.log(data);
+  //   res.writeHead(200, httpHelpers.headers);
+  //   res.end(data);
+  // });
+  // *** LOOK AT MIME-TYPES LIBRARY FOR GRABBING THE CSS ** 
+  callback(asset, function(err, data) {
+    res.writeHead(200, exports.headers);
+    res.write(data);
+  });
+
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,59 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if(req.method === 'GET' && req.url === '/'){
+    // function writeIndex(data){
+    //   res.writeHead(200, exports.headers);
+    //   res.write(data);
+    //   res.end();
+    // }
+    // var callback = writeIndex.bind(this);
+    //httpHelpers.serveAssets(res, '/index.html', callback);
+    // res.writeHead(200, httpHelpers.headers);
+    httpHelpers.serveAssets(res, archive.paths.siteAssets + '/index.html', fs.readFile);
+  } else if(req.method === 'POST'){
+    //go to sites.txt
+    //console.log("got a post request");
+    var storage = [];
+    req.on('data', function(chunk){
+      storage.push(chunk);
+    });
+    req.on('end', function(){
+      storage = Buffer.concat(storage).toString();
+      console.log(storage);
+    });
+
+    /*console.log('input data: ' + storage);
+    var sitesList;
+    fs.readFile(archive.paths.list, function(err, data){//look at datafile
+      if(!data){
+        sitesList = '{}';
+      }else{
+        sitesList = data;
+      }
+      console.log(sitesList);
+    })//fs readFile
+    sitesList = JSON.parse(sitesList);//parse
+    if(sitesList[searchTerm] === undefined){  //if not found
+      sitesList[searchTerm] = false;// add entry to sites.txt
+      res.writeHead(404, httpHelpers.headers);// 404
+      res.write('Not Found');
+    }else if(sitesList[searchTerm]){//if found & in sites folder
+      httpHelpers.serveAssets(res, archive.paths.archivedSites + '/' + searchTerm, fs.readFile);// serveAssets 302
+    }else if(!sitesList[searchTerm]){//if found & not in sites folder
+      res.writeHead(404, httpHelpers.headers);// 404
+      res.write('Site has yet to be retrieved');
+    }
+    fs.writeFile(archive.paths.list, JSON.stringify(sitesList));
+        //fs write file
+*/
+  }
+
+  setTimeout(() => {
+    res.end();
+  }, 10);//res.end(archive.paths.list);
 };
